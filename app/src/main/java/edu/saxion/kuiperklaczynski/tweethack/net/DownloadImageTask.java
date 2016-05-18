1124767e6dbd927b9d8446519fea845298854e38
DireQtDesigns <direqtdesigns@gmail.com>
2016-05-18 12:00:28 +0200

    US Null, 3-5-2016, Did another thing, even more things this time

--- app/src/main/java/edu/saxion/kuiperklaczynski/tweethack/net/DownloadImageTask.java
+++ app/src/main/java/edu/saxion/kuiperklaczynski/tweethack/net/DownloadImageTask.java
@@ -2,30 +2,24 @@ package edu.saxion.kuiperklaczynski.tweethack.net;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ImageView;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.nio.channels.FileLockInterruptionException;
 
 /**
  * Created by leonk on 27/04/2016.
  */
 public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
 
-    ImageView bmImage;
+    ImageView imageViewToApplyTo;
     String urldisplay;
     public static final String TAG = "TweetHax_TwListAdapter"; //Log Tag
     boolean success = false;
 
     public DownloadImageTask(ImageView bmImage) {
-        this.bmImage = bmImage;
+        this.imageViewToApplyTo = bmImage;
     }
 
     protected Bitmap doInBackground(String... urls) {
@@ -43,6 +37,6 @@ public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
     }
 
     protected void onPostExecute(Bitmap result) {
-        if(success) bmImage.setImageBitmap(result);
+        if(success) imageViewToApplyTo.setImageBitmap(result);
     }
 }
